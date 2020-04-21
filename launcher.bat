f8693210b5d4c337c264ea8c7b58403f0080910d
Bohdan Tkachenko <bohdantk@buffalo.edu>
2020-04-21 10:25:50 -0400

    - launcher.bat: Fixed path

--- launcher.bat
+++ launcher.bat
@@ -1,14 +1,15 @@
-:: Change directory here to match file location:
+:: #Change directory here to match file location:
 
-::cd C:\Users\tubxt2p\Documents\Python\python-projects\tictactoe\
-C:\Users\bohda\Desktop\Programming\Python\python-projects\disc-file-app\
-
+cd C:\Users\tubxt2p\Documents\Python\python-projects\disc-file-app
 
 
-:: Change file name here
+
+:: #Change file name here
 
 python disc_file_app.py
 ::python tests.py
 
-:: Transfer file to w-drive for SQL Server Import
+
+
+:: #Transfer file to w-drive for SQL Server Import
 MOVE "C:\Users\tubxt2p\Documents\Python\python-projects\disc-file-app\MfDiscItems*txt" "W:\DecisionSupport\SQL\MfDiscItems\"
